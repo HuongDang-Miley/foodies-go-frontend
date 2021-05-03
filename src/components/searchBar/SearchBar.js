e8fdb3df492bb7b02031b1197c1dec05acdc43a8
Miley Dawn <mileydawn@huongs-mbp.myfiosgateway.com>
2021-05-03 06:37:17 -0400

    style search box

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,29 +1,47 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+import './searchBar.css'
 import { connect } from "react-redux";
 import { useHistory } from 'react-router-dom';
 import { getNearbySearch, togglePlaceDetail, getSearchWord } from '../../stores/actions/searchActionCreator'
+import { Button, TextField, Link, Container, Typography } from '@material-ui/core'
+import useStyles from '../../stores/Theme.js'
 
 function SearchBar(props) {
+    const classes = useStyles()
 
     const history = useHistory()
     let searchRef = useRef()
+    const [keyWord, setKeyWord] = useState('')
+    const [value, setValue] = useState('')
 
     const handleSearch = (event) => {
-        event.preventDefault()
-        props.togglePlaceDetail(false)
-        props.getNearbySearch(searchRef.current.value, props.userLatLng)
-        props.getSearchWord(searchRef.current.value)
-        searchRef.current.value = ''
-        history.push('./')
-    }
+                event.preventDefault()
+                props.togglePlaceDetail(false)
+                props.getNearbySearch(searchRef.current.value, props.userLatLng)
+                props.getSearchWord(searchRef.current.value)
+                searchRef.current.value = ''
+                history.push('./')
+            }
 
     return (
-        <form onSubmit={handleSearch}>
+        <form onSubmit={handleSearch}  className='search-wrapper'>
             <input
+            className='search-input'
                 ref={searchRef}
                 placeholder='Search Place'>
             </input>
-            <button>Search</button>
+            {/* <TextField
+                value={keyWord}
+                className={classes.field}
+                // type='email'
+                onChange={(e) => setValue({ keyWord: e.target.value })}
+            // label="Email"
+            // variant="outlined"
+            // fullWidth
+            // required
+            // error={emailError}
+            />
+            <button>Search</button> */}
         </form>
     )
 }
